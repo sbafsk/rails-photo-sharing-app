1f52c34f7990add9a355ef544e3a0762e5f480c3
sbafsk <razor1836@gmail.com>
2021-10-19 15:33:33 -0300

    add scaffold for posts, created relation posts with user

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :posts
+  resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root 'posts#index'
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destrol'
+  get 'signup', to: 'users#new'
+  get 'sessions', to: 'sessions#new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
